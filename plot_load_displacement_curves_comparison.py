39e0a28b6c7db56003697682deab0b25d2fa9e91
Prajwal.K.A <prajwalka995@gmail.com>
2020-12-30 12:08:36 -0800

    Adding efem results-1

--- plot_load_displacement_curves_comparison.py
+++ plot_load_displacement_curves_comparison.py
@@ -38,6 +38,24 @@ for color , (f, lab) in zip (colors_rankine, files_gradient_enhanced_rankine.ite
     ax.plot(U, RF, label=lab, c = color)
 # end plot gradient_enhanced_rankine curves
 
+# plot the efem results
+files_efem = {'./embedded_fem/efem_lin_coarse_mode2.csv' : r'efem-lin-coarse-mesh-mode2',
+              './embedded_fem/efem_lin_coarse_mixed.csv' : r'efem-lin-coarse-mesh-mixed',
+              './embedded_fem/efem_lin_medium_mixed.csv' : r'efem-lin-medium-mesh-mixed',              
+			  './embedded_fem/efem_lin_fine_mixed.csv' : r'efem-lin-fine-mesh-mixed',}
+
+colors_efem = plt.cm.Reds_r(np.linspace(0.0, 0.7, len(files_efem) ))
+
+for color , (f, lab) in zip(colors_efem, files_efem.items()):
+    
+    data = np.loadtxt ( f, delimiter=' ', skiprows=0)
+
+    U = data[:,0]
+    RF = data[:,1]
+
+    ax.plot(U, RF, label=lab, c = color, linestyle = '--')
+# end plot embedded_fem curves
+
 # make plots readable
 ax.set_xlabel('shear (mm)')
 ax.set_ylabel('reaction force (N)')
